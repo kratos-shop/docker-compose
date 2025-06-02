7214232059d2d948e7cc52b2fc565004a4bf2b9c
shihao <2544528304@qq.com>
2025-06-02 13:56:30 +0800

    feature: mysql

--- compose/mysql/init.sql
+++ compose/mysql/init.sql
@@ -2,4 +2,6 @@
 set session transaction isolation level read committed;
 SET GLOBAL tx_isolation='READ-COMMITTED';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'Eisoo.com123' WITH GRANT OPTION;
+CREATE USER canal IDENTIFIED BY 'canal';  
+GRANT SELECT, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'canal'@'%';
 FLUSH PRIVILEGES;
